c95d635788a9a27917167e42dc3ec0171200b7fc
maltiez2 <maltiez2@gmail.com>
2024-05-10 00:04:49 +0300

    wip

--- Bullseye/Bullseye/source/BullseyeModSystem.cs
+++ Bullseye/Bullseye/source/BullseyeModSystem.cs
@@ -1,6 +1,8 @@
-﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+﻿using Bullseye.Old;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
 namespace Bullseye;
@@ -12,7 +14,13 @@ internal class BullseyeModSystem : ModSystem
 
     public override void Start(ICoreAPI api)
     {
-
+        /*api.RegisterEntityBehaviorClass("Bullseye.aimingaccuracy", typeof(BullseyeEntityBehaviorAimingAccuracy));
+        api.RegisterCollectibleBehaviorClass("Bullseye.Throwable", typeof(BullseyeCollectibleBehaviorThrowable));
+        api.RegisterCollectibleBehaviorClass("Bullseye.Bow", typeof(BullseyeCollectibleBehaviorBow));
+        api.RegisterCollectibleBehaviorClass("Bullseye.Spear", typeof(BullseyeCollectibleBehaviorSpear));
+        api.RegisterCollectibleBehaviorClass("Bullseye.Sling", typeof(BullseyeCollectibleBehaviorSling));
+        api.RegisterCollectibleBehaviorClass("Bullseye.ThrowableStone", typeof(BullseyeCollectibleBehaviorThrowableStone));
+        api.RegisterCollectibleBehaviorClass("Bullseye.Ammunition", typeof(BullseyeCollectibleBehaviorAmmunition));*/
     }
 
     public override void StartClientSide(ICoreClientAPI api)
@@ -21,6 +29,9 @@ internal class BullseyeModSystem : ModSystem
         AimingSystem = new(api);
         Synchronizer = new RangedWeapon.SynchronizerClient(api);
         HarmonyPatches.Patch(_harmonyId, AimingSystem);
+
+        api.Input.RegisterHotKey("bullseye.ammotypeselect", Lang.Get("bullseye:select-ammo"), GlKeys.F, HotkeyType.GUIOrOtherControls);
+        api.Gui.RegisterDialog(new AmmoSelectDialog(api));
     }
 
     public override void StartServerSide(ICoreServerAPI api)
